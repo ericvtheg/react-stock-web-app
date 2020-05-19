5e1a2635f82e41f4724f8a1c81e82a4b037f457e
Eric Ventor <eventor@Erics-MacBook-Air.local>
2020-05-18 20:29:09 -0700

    added path to express

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const logger = require('./middleware/logger');
 const corsOptions = require('./middleware/cors');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 const port = 3001;
 
@@ -9,7 +10,10 @@ app.use(logger);
 app.use(cors(corsOptions));
 app.use('/', require('./routes/routes'));
 
-
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(port, () => console.log(
     `Stock Tickr listening at http://localhost:${port}`
